d3db2fa65fe610e321da644ab0a14758659d66db
tigra96 <tagsasha@yandex.ru>
2018-04-22 21:46:04 +0300

    Add normalization.py containing normalization and lemmatization

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
-from bs4      import BeautifulSoup as BS
+from normalization import normalization
+
 
 def main():
     df = pd.read_excel('body.xlsx')
-    df['Post'] = df.body.apply(
-        lambda x: '.'.join(content.text.lower() for content in BS(x).find_all(['p', 'li', 'h1'])))
+
+    df = normalization(df)
+    print(df.head())
 
     return
 
---
+++ normalization.py
@@ -0,0 +1,26 @@
+import pymorphy2
+
+from bs4 import BeautifulSoup as BS
+
+def normalization(df):
+
+    df['Post'] = df['body'].apply(
+        lambda x: '.'.join(content.text.lower() for content in BS(x).find_all(['p', 'li', 'h1'])))
+
+    df['Post_norm'] = df['Post'].apply(lemmatization)
+
+    return df
+
+def lemmatization(name):
+
+    morph = pymorphy2.MorphAnalyzer()
+    
+    letters = [i for i in 'йцукенгшщзхъфывапролджэёячсмитьбю .,']
+    name = name.replace('.', '. ').replace(',', ', ')
+    
+    name = ''.join(i for i in name if i in letters)
+    name = name.replace('.', '').replace(',', '')
+    
+    name = ' '.join([morph.parse(i)[0].normal_form for i in name.split(' ')[:20]])
+    name += ' '
+    return name
